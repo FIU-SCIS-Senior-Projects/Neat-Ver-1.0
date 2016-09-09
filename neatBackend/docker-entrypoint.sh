39bbcb893d53f4d00a1172d8bcd71dcb4e428d99
Pachev Joseph <pachevjoseph@gmail.com>
2016-09-09 18:56:37 -0400

    Update docker-entrypoint.sh

--- neatBackend/docker-entrypoint.sh
+++ neatBackend/docker-entrypoint.sh
@@ -8,7 +8,7 @@ python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
 # Create a superuser
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password123')" | python manage.py shell
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
