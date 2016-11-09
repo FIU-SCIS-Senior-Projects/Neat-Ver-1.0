cf099f2955cf60d5c9558659652a3a2b8e682d3e
WhitePhantom <WhitePhantom@WhitePhantoms-Mac.local>
2016-11-09 08:23:33 -0500

    Added onRecieve method

--- NeatMobileApp/src/components/ChatRoom.js
+++ NeatMobileApp/src/components/ChatRoom.js
@@ -7,6 +7,7 @@ class ChatRoom extends Component {
     this.state = {messages: []};
     this.onSend = this.onSend.bind(this);
   }
+
   componentWillMount() {
     this.setState({
       messages: [
@@ -16,20 +17,41 @@ class ChatRoom extends Component {
           createdAt: new Date(Date.now()),
           user: {
             _id: 2,
-            name: 'React Native',
+            name: 'Neat Help',
             avatar: 'https://facebook.github.io/react/img/logo_og.png',
           },
         },
       ],
     });
   }
+
   onSend(messages = []) {
     this.setState((previousState) => {
+      //TODO: This is where we send a message to ChatBot API
       return {
         messages: GiftedChat.append(previousState.messages, messages),
       };
     });
   }
+
+  onReceive(text) {
+    this.setState((previousState) => {
+      //TODO: This is where a message needs to be sent to ChatBot API
+      return {
+        messages: GiftedChat.append(previousState.messages, {
+          _id: Math.round(Math.random() * 1000000),
+          text: text,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            // avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        }),
+      };
+    });
+  }
+
   render() {
     return (
       <GiftedChat
